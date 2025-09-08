1d4fca29fb37a7156c59437b30d8ab5862dcb541
Nicholas Genta <58099250+mbahmu25@users.noreply.github.com>
2025-09-08 22:14:18 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ cd src
 # gcc main.c smoothing.c gdalShortcut.c -o ../main $(gdal-config --cflags) $(gdal-config --libs) -lm -lopen
 gcc main.c transformation.c smoothing.c gdalShortcut.c -o ../main $(gdal-config --cflags) $(gdal-config --libs) -lm
 cd ../
+mkdir -p data
+mkdir -p result
 cp asli.tif data/asli.tif
 time ./main data/asli.tif data/lahan.tif result/result_9.tif 250 -7.5352051 112.6938265 -7.5355800 112.6941597 75 0
 # time ./main data/tess5.tif data/lahan.tif 300
